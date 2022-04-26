9b25eb69d70a2226fe513cf1920d64bc766ac130
Sam Smith <samlsmith424@gmail.com>
2022-04-26 10:22:11 -0700

    Test: create user session

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user), status: :ok
+    else
+      render json: { error: "bad credentials" }, status: :bad_request
+    end
+  end
+end
