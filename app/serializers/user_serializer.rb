8f64ae77b4beef3c6c445524e5878fdf3cea5350
Sam Smith <samlsmith424@gmail.com>
2022-05-21 23:27:54 -0700

    Refactor: delete commented out code

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,16 +3,4 @@ class UserSerializer
   set_id :id
   set_type :users
   attributes :email, :auth_token
-  # def self.new(user)
-  #   {
-  #     "data": {
-  #       "type": "users",
-  #       "id": user.id.to_s,
-  #       "attributes": {
-  #         "email": user.email,
-  #         "api_key": user.api_key
-  #       }
-  #     }
-  #   }
-  # end
 end
