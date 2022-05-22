968d0d8eef52f546d0f59aa417f69dd38bf23301
Sam Smith <samlsmith424@gmail.com>
2022-05-21 23:24:44 -0700

    Refactor: delete commented out code

--- spec/facades/open_weather_facade_spec.rb
+++ spec/facades/open_weather_facade_spec.rb
@@ -8,14 +8,4 @@ RSpec.describe 'open weather facade' do
       expect(weather_info).to be_an(ForecastPoro)
     end
   end
-
-  # it 'can return future weather data from latitude and longitude' do
-  #   VCR.use_cassette("open weather facade") do
-  #     travel_time = "1:00:00"
-  #     destination_coords = OpenWeatherService.find_weather(39.738453, -104.984853)
-  #     weather_info = OpenWeatherFacade.get_future_weather(travel_time, destination_coords)
-  #
-  #     expect(weather_info).to be_an(ForecastPoro)
-  #   end
-  # end
 end
